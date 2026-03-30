57fdba712b59b7306cd65b6c5a0107957c78b54d
coneill <coneill@towson.edu>
2026-03-30 14:38:04 -0400

    added in the vercel config json file and setup the error boundary wrapper for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
+// Modules
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Components
+import ErrorBoundary from "@/components/ErrorBoundary/ErrorBoundary";
+
+// Pages
 import HomePage from "@/pages/HomePage";
 import DeckBuilderPage from "@/pages/DeckBuilderPage";
-import DeckDetailPage from "@/pages/DeckDetalPage";
+import DeckDetailPage from "@/pages/DeckDetailPage";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/build" element={<DeckBuilderPage />} />
-        <Route path="/build/:deckId" element={<DeckBuilderPage />} />
-        <Route path="/deck/:deckId" element={<DeckDetailPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/build" element={<DeckBuilderPage />} />
+          <Route path="/build/:deckId" element={<DeckBuilderPage />} />
+          <Route path="/deck/:deckId" element={<DeckDetailPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 }
